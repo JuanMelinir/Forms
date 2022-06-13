efb7f6940d62b7e9e8ea06265186f2c648c9737b
JuanMelinir <juan.melinir@est.fi.uncoma.edu.ar>
2022-06-13 20:27:15 -0300

    forms

---
+++ forms/login.php
@@ -0,0 +1,15 @@
+<div class="container-fluid align-self-center justify-content-center h-100" style="height:100%;">
+<div class='container col-md-3 '>
+<div class='col-auto mb-3'>
+<label class='form-label d-flex justify-content-center align-items-center' for='autoSizingInput'>Nombre</label>
+<input type='text' class='form-control' id='txtid' placeholder=''>
+</div>
+<div class='col-auto mb-3'>
+<label class='form-label d-flex justify-content-center align-items-center' for='autoSizingInput'>Contraseña</label>
+<input type='password' class='form-control' id='passid' placeholder=''>
+</div>
+<div class='d-flex justify-content-center align-items-center  mb-3'>
+<button type='button' class='btn btn-primary'>Login</button>
+</div>
+</div>
+</div>
---
+++ forms/nombreyapellido.php
@@ -0,0 +1,12 @@
+<form class='row row-cols-2 align-items-center'>
+<div class='row align-self-center'>
+<div class='col-auto'>
+<label class='form-label' for='autoSizingInput'>Nombre</label>
+<input type='text' class='form-control' id='txt$id' placeholder=''>
+</div>
+<div class='col-auto'>
+<label class='form-label' for='autoSizingInput'>Apellido</label>
+<input type='text' class='form-control' id='txt$id' placeholder=''>
+</div>
+</div>
+</form>
