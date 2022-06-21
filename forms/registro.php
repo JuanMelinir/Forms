907176a05add4462332844a7064a390c5723e0b0
JuanMelinir <juan.melinir@est.fi.uncoma.edu.ar>
2022-06-21 17:24:30 -0300

    agregando cmentarios

--- forms/registro.php
+++ forms/registro.php
@@ -1,12 +1,12 @@
 <!--STEP 2-->
-<!--inicio container-->
+<!--inicio container principal-->
 <div class="container-fluid vh-100 " id="body">
 <!--inicio fila-->
 <div class='row'>
 <?php
 include 'form.php';
 ?>
-
+<!--Navbar vertical-->
     <div class="menu__side" id="menu_side">
     <a href="#" class="selected">
         <div class="name__page">
@@ -17,58 +17,47 @@ include 'form.php';
 
             <a href="#" class="selected">
                 <div class="option">
-                    <i class="fas fa-home" title="Inicio"></i>
-                    <h4>Inicio</h4>
+                    <i class="fa fa-circle-o" aria-hidden="true" title="Step 1"></i>
+                    <h4>Step 1</h4>
                 </div>
             </a>
 
             <a href="#">
                 <div class="option">
-                    <i class="far fa-file" title="Portafolio"></i>
-                    <h4>Portafolio</h4>
+                    <i class="fa fa-circle-o" aria-hidden="true" title="Step 2"></i>
+                    <h4>Step 2</h4>
                 </div>
             </a>
             
             <a href="#">
                 <div class="option">
-                    <i class="fas fa-video" title="Cursos"></i>
-                    <h4>Cursos</h4>
+                    <i class="fa fa-circle-o" aria-hidden="true"title="Step 3"></i>
+                    <h4>Step 3</h4>
                 </div>
             </a>
 
             <a href="#">
                 <div class="option">
-                    <i class="far fa-sticky-note" title="Blog"></i>
-                    <h4>Blog</h4>
+                    <i class="fa fa-circle-o" aria-hidden="true" title="Step 4"></i>
+                    <h4>Step 4</h4>
                 </div>
             </a>
 
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-id-badge" title="Contacto"></i>
-                    <h4>Contacto</h4>
-                </div>
-            </a>
-
-            <a href="#">
-                <div class="option">
-                    <i class="far fa-address-card" title="Nosotros"></i>
-                    <h4>Nosotros</h4>
-                </div>
-            </a>
 
         </div>
 </div>
     </div>
-<!--fin row-->
+<!--fin navbar vertical-->
 
 <?php
+//Llamo a la libreria de forms
 $form=new Form();
-
+//Inicio columna contenedora
 echo $form->createCol("step1"," d-flex justify-content-center align-items-center vh-100",
-
+//Inicio columna contenedora de inputs
 $form->createCol("","-8",
 
+//Inicio contenedor step
 $form->createStepper("","",
 $form->createStepperItem(""," completed","1",null).
 $form->createStepperItem("","","2",null).
@@ -76,44 +65,56 @@ $form->createStepperItem("","","3",null).
 $form->createStepperItem("","","4",null).
 $form->createStepperItem("","","5",null)
 ,"",null).
-
+//fin contenedor step
+//Inicio fila contenedora de inputs
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-
+//Inicio columna contenedora de input
 $form->createCol("grupo__nombre"," mb-3 ms-3",
 $form->createRow(""," formulario__grupo-input",
 $form->createInput("Nombre","nombre","txt","","","",null).
 $form->createIcon("","formulario__validacion-estado fas fa-times-circle","",null),
 null).
+//parrafo que aparece cuando hay algún error
 $form->createP("","formulario__input-error","El nombre debe contener de 1 a 16 letras.",null)
 ).
-
-$form->createCol(""," mb-3 ms-3",
-$form->createRow("grupo__apellido"," formulario__grupo-input",
+//Fin columna contenedora de input
+//Inicio columna contenedora de input
+$form->createCol("grupo__apellido"," mb-3 ms-3",
+$form->createRow(""," formulario__grupo-input",
 $form->createInput("Apellido","apellido","txt","","","",null).
 $form->createIcon("","formulario__validacion-estado fas fa-times-circle","",null),
 null).
+//parrafo que aparece cuando hay algún error
 $form->createP("","formulario__input-error","El apellido debe contener de 1 a 16 letras.",null)
 ),
+//Fin columna contenedora de input
 null).
-
+//fin fila contenedora de inputs
+//Inicio fila contenedora de inputs
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",
-$form->createRow("grupo__nacionalidad"," formulario__grupo-input",
+//Inicio columna contenedora de input
+$form->createCol("grupo__nacionalidad"," mb-3 ms-3",
+$form->createRow(""," formulario__grupo-input",
 $form->createInput("Nacionalidad","nacionalidad","txt","","","",null).
-$form->createIcon("","formulario__validacion-estado fas fa-times-circle","",null).
-$form->createP("","formulario__input-error","La nacionalidad tiene que ser de 4 a 16 dígitos y solo puede contener letras.",null),
-null)
+$form->createIcon("","formulario__validacion-estado fas fa-times-circle","",null),
+null).
+//parrafo que aparece cuando hay algún error
+$form->createP("","formulario__input-error","La nacionalidad tiene que ser de 4 a 16 dígitos y solo puede contener letras.",null)
 ).
-
-$form->createCol(""," mb-3 ms-3",
+//Fin columna contenedora de input
+//Inicio columna contenedora de input
+$form->createCol("grupo__fecha"," mb-3 ms-3",
 $form->createRow(""," formulario__grupo-input",
 $form->createInput("Fecha de nacimiento","fecha","date","","","",null).
-$form->createIcon("","formulario__validacion-estado fas fa-times-circle","",null).
-$form->createP("","formulario__input-error","Debe seleccionar una fecha.",null),
-null)
-),
-
+$form->createIcon("","formulario__validacion-estado fas fa-times-circle","",null),
 null).
+//parrafo que aparece cuando hay algún error
+$form->createP("","formulario__input-error","Debe seleccionar una fecha.",null),
+),
+//Fin columna contenedora de input
+null).
+//Fin fila contenedora de inputs
+//Inicio fila contenedora de inputs
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
 
 $form->createDiv("","d-flex justify-content-center align-items-center  mb-3",
@@ -123,9 +124,9 @@ $form->createLink("","#step2","",$form->createButton("Siguiente","btnSiguiente",
 ,null)
   
 ,null)
-
+//Fin columna contenedora de inputs
 ,null)
-
+//Fin columna contenedora
 ?>
 
 
@@ -133,9 +134,9 @@ $form->createLink("","#step2","",$form->createButton("Siguiente","btnSiguiente",
 </div>
 <?php
 $form=new Form();
-
+//Inicio columna contenedora
 echo $form->createCol("step2"," d-flex justify-content-center align-items-center vh-100",
-
+//Inicio columna contenedora de inputs
 $form->createCol("","-8",
 
 $form->createStepper("","",
@@ -145,17 +146,31 @@ $form->createStepperItem("","","3",null).
 $form->createStepperItem("","","4",null).
 $form->createStepperItem("","","5",null)
 ,"",null).
-
+//Inicio fila contenedora de inputs
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createSelect("Provincia","selectProvincia","","",["Neuquén","Río Negro","Mendoza","La Pampa","Chubut"],null)).
-
-$form->createCol(""," mb-3 ms-3",$form->createSelect("Ciudad","selectCiudad","","",["Neuquén","Chos malal","Aluminé","Andacollo","Añelo"],null)),
+//Inicio columna contenedora de input
+$form->createCol(""," mb-3 ms-3",$form->createSelect("Provincia","selectProvincia","","",["Neuquén","Río Negro","Mendoza","La Pampa","Chubut"],null)
+).
+//Fin columna contenedora de input
+//Inicio columna contenedora de input
+$form->createCol(""," mb-3 ms-3",$form->createSelect("Ciudad","selectCiudad","","",["Neuquén","Chos malal","Aluminé","Andacollo","Añelo"],null)
+),
+//Fin columna contenedora de input
 null).
-
+//Fin fila contenedora de inputs
+//Inicio fila contenedora de inputs
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Codigo postal","inputCodigoPostal","number","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Domicilio","inputDomicilio","txt","","","",null)),
+//Inicio columna contenedora de input
+$form->createCol(""," mb-3 ms-3",$form->createInput("Codigo postal","inputCodigoPostal","number","","","",null)
+).
+//Fin columna contenedora de input
+//Inicio columna contenedora de input
+$form->createCol(""," mb-3 ms-3",$form->createInput("Domicilio","inputDomicilio","txt","","","",null)
+),
+//Fin columna contenedora de input
 null).
+//Fin fila contenedora de inputs
+//Inicio columna contenedora de input
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
 
 $form->createDiv("","d-flex justify-content-center align-items-center  mb-3",
@@ -173,9 +188,9 @@ null),
 
 ,null).
 
-
+//Inicio columna contenedora
 $form->createCol("step3"," d-flex justify-content-center align-items-center vh-100",
-
+//Inicio columna contenedora de inputs
 $form->createCol("","-8",
 
 $form->createStepper("","",
@@ -187,13 +202,23 @@ $form->createStepperItem("","","5",null)
 ,"",null).
 
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Telefono","inputTelefono","number","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Email","inputEmail","email","","","",null)),
+$form->createCol(""," mb-3 ms-3",$form->createInput("Telefono","inputTelefono","number","","","",null)
+).
+//Fin columna contenedora de input
+$form->createCol(""," mb-3 ms-3",$form->createInput("Email","inputEmail","email","","","",null)
+),
+//Fin columna contenedora de input
 null).
 
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nivel de estudios","inputEstudios","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Estado","inputEstado","txt","","","",null)),
+//Inicio columna contenedora de input
+$form->createCol(""," mb-3 ms-3",$form->createInput("Nivel de estudios","inputEstudios","txt","","","",null)
+).
+//Fin columna contenedora de input
+//Inicio columna contenedora de input
+$form->createCol(""," mb-3 ms-3",$form->createInput("Estado","inputEstado","txt","","","",null)
+),
+//Fin columna contenedora de input
 null).
 $form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
 
@@ -202,7 +227,7 @@ $form->createLink("","#step2","",
 $form->createButton("Anterior","btnAnterior","button","btn btn-primary me-3",null),
 null).
 $form->createLink("","#step4","",
-$form->createButton("Siguiente","btnSiguiente","button","btn btn-primary ms-3",null),
+$form->createButton("Finalizar","btnFinalizar","button","btn btn-primary ms-3",null),
 null),
  null)
 
@@ -211,148 +236,10 @@ null),
 ,null)
 
 
-,null).
-
-$form->createCol("step4"," d-flex justify-content-center align-items-center vh-100",
-
-$form->createCol("","-8",
-
-$form->createStepper("","",
-$form->createStepperItem(""," completed","1",null).
-$form->createStepperItem(""," completed","2",null).
-$form->createStepperItem(""," completed","3",null).
-$form->createStepperItem(""," completed","4",null).
-$form->createStepperItem("","","5",null)
-,"",null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nombre","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Apellido","textApellido","txt","","","",null)),
-null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nacionalidad","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Fecha de nacimiento","date","date","","","",null)),
-null).
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-
-$form->createDiv("","d-flex justify-content-center align-items-center  mb-3",
-$form->createLink("","step2","",$form->createButton("Siguiente","btnSiguiente","button","btn btn-primary",null),null),
- null)
-
-,null)
-
 ,null)
 
 
-,null).
-
-
-$form->createCol("step5"," d-flex justify-content-center align-items-center vh-100",
-
-$form->createCol("","-8",
-
-$form->createStepper("","",
-$form->createStepperItem(""," completed","1",null).
-$form->createStepperItem(""," completed","2",null).
-$form->createStepperItem(""," completed","3",null).
-$form->createStepperItem(""," completed","4",null).
-$form->createStepperItem(""," active","5",null)
-,"",null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nombre","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Apellido","textApellido","txt","","","",null)),
-null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nacionalidad","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Fecha de nacimiento","date","date","","","",null)),
-null).
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-
-$form->createDiv("","d-flex justify-content-center align-items-center  mb-3",
-$form->createLink("","step2","",$form->createButton("Siguiente","btnSiguiente","button","btn btn-primary",null),null),
- null)
-
-,null)
-
-,null)
-
-
-,null).
-
-$form->createCol("step6"," d-flex justify-content-center align-items-center vh-100",
-
-$form->createCol("","-8",
-
-$form->createStepper("","",
-$form->createStepperItem(""," active","1",null).
-$form->createStepperItem("","","2",null).
-$form->createStepperItem("","","3",null).
-$form->createStepperItem("","","4",null).
-$form->createStepperItem("","","5",null)
-,"",null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nombre","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Apellido","textApellido","txt","","","",null)),
-null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nacionalidad","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Fecha de nacimiento","date","date","","","",null)),
-null).
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-
-$form->createDiv("","d-flex justify-content-center align-items-center  mb-3",
-$form->createLink("","step2","",$form->createButton("Siguiente","btnSiguiente","button","btn btn-primary",null),null),
- null)
-
-,null)
-
-,null)
-
-
-,null).
-
-
-$form->createCol("step7"," d-flex justify-content-center align-items-center vh-100",
-
-$form->createCol("","-8",
-
-$form->createStepper("","",
-$form->createStepperItem(""," active","1",null).
-$form->createStepperItem("","","2",null).
-$form->createStepperItem("","","3",null).
-$form->createStepperItem("","","4",null).
-$form->createStepperItem("","","5",null)
-,"",null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nombre","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Apellido","textApellido","txt","","","",null)),
-null).
-
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-$form->createCol(""," mb-3 ms-3",$form->createInput("Nacionalidad","txtNombre","txt","","","",null)).
-$form->createCol(""," mb-3 ms-3",$form->createInput("Fecha de nacimiento","date","date","","","",null)),
-null).
-$form->createRow(""," mb-4 ms-4 justify-content-center align-items-center",
-
-$form->createDiv("","d-flex justify-content-center align-items-center  mb-3",
-$form->createLink("","step2","",$form->createButton("Siguiente","btnSiguiente","button","btn btn-primary",null),null),
- null)
-
-,null)
-
-,null)
-
-
-,null)
-
-
-
+//Fin container principal
 
 
 ?>
